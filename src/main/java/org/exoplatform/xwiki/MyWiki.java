ce30f065bc7c35da920b656e0336c85752488f32
Khemais <kmenzli@exoplatform.com>
2016-05-14 12:48:07 +0100

    Init

---
+++ src/main/java/org/exoplatform/xwiki/MyWiki.java
@@ -0,0 +1,17 @@
+package org.exoplatform.xwiki;
+
+import org.xwiki.component.embed.EmbeddableComponentManager;
+
+/**
+ * Created by kmenzli on 13/05/16.
+ */
+public class MyWiki {
+
+    public static void main (String args []) {
+
+        // Initialize Rendering components and allow getting instances
+        EmbeddableComponentManager componentManager = new EmbeddableComponentManager();
+
+
+    }
+}
